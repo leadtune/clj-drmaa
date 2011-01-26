ab600bcfd512893917892a56ddcbfed4ef292b72
Ben Mabey <ben@benmabey.com>
2011-01-26 16:35:54 -0700

    adds deps to project.clj rather than relying on the ubuntu packages
    This change was mostly to ease development pain on projects that require
    this one.

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject clj-drmaa "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "drmaa client"
+  :repositories {"leadtune-repo" "http://c0026236.cdn1.cloudfiles.rackspacecloud.com/repo"}
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [lt/drmaa "ubuntu-version"]
+                 [lt/jgdi "ubuntu-version"]
+                 [lt/juti "ubuntu-version"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
-                     [midje "1.0.0RC1"]])
+                     [midje "1.0.1"]])
